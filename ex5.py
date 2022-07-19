6ef7f75538ca1982abd55d53f68b38ac52b264cd
AMALNR98 <amalnr123@gmail.com>
2022-07-19 11:41:13 +0530

    ex5 completed

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Amal N R'
+my_age = 24 #satyayittum
+my_height = 64 #inches
+my_weight = 158 #lbs
+my_eyes = 'black'
+my_teeth ='white'
+my_hair = 'black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy")
+print("Actually thats not too heavy.")
+print(f"He's got {my_eyes} and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} deepending on the coffee")
+
+#this line is tricky, try to get it exatly right
+total = my_age + my_height + my_weight
+print(f" If I dd {my_age}, {my_height}, and {my_weight} I get {total}.")
