1841d42d7984e3eb18312c273ce90107ae441ee7
AMALNR98 <amalnr123@gmail.com>
2022-07-20 10:56:09 +0530

    ex9 added to Exercise

--- Exercise.py
+++ Exercise.py
@@ -109,4 +109,16 @@ print(formatter.format(
     "Own text here",
     "Maybe a poem",
     "Or a song about fear"
-))
+))#.Here's some new strange dtuff, remeber type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months ="jan\nFeb\nMar\nApr\nMay\nJun\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months:", months)
+
+print("""
+There's something going on there.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want. or 5, or 6""")
