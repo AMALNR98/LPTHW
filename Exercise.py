08f04ff9bd798e79bf7d195f2fe1ea7f551b7fcd
AMALNR98 <amalnr123@gmail.com>
2022-07-20 15:04:58 +0530

    ex15 completed

--- Exercise.py
+++ Exercise.py
@@ -177,3 +177,20 @@ Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice.
 """)
+# Readin file
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}")
+print(txt.read())
+
+print("Type the file name again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+
